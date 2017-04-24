5b035728d658242368f03d504b72810fe5878f63
vaibhavchellani <vaibhavchellani223@gmail.com>
2017-04-24 12:06:45 +0530

    removed auth from master will add later

--- app/src/main/java/com/example/vaibhavchellani/pokerpaisa/MainActivity.java
+++ app/src/main/java/com/example/vaibhavchellani/pokerpaisa/MainActivity.java
@@ -17,8 +17,6 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.message) TextView mTextMessage;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -49,32 +47,8 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener=new FirebaseAuth.AuthStateListener(){
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user=firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d("from onAuthStateChanged", "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d("onAuthStateChanged", "onAuthStateChanged:signed_out");
-                }
-            }
-        };
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
+
 }
